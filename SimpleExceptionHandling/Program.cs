d6e19637f6f9328b672033be4751dd32e20cae18
lynda-nneka <86114880+lynda-nneka@users.noreply.github.com>
2022-11-29 09:55:32 +0100

    Add files via upload

---
+++ SimpleExceptionHandling/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using SimpleExceptionHandling;
+
+public static class Program
+{
+    static void Main(string[] args)
+    {
+        ExceptionHandling exception = new ExceptionHandling();
+        exception.userAuthentication();
+
+    }
+}
---
+++ SimpleExceptionHandling/SimpleExceptionHandling.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SimpleExceptionHandling
+{
+    public class ExceptionHandling
+    {
+        public void userAuthentication()
+        {
+            Regex regex = new Regex("[^a-zA-Z ]");
+            try
+            {
+               Console.WriteLine("Enter your full name");
+               string userName = Console.ReadLine().Trim();
+    
+                if (regex.IsMatch(userName)) throw new InvalidDataException
+                ("please enter your name in the correct format");
+
+                Console.WriteLine("Enter your age: ");
+                int userAge = Convert.ToInt32(Console.ReadLine());
+
+                if (userAge <= 18)
+                {
+                    Console.WriteLine("Can't view this show,This show is rated 18+");
+                }
+                else
+                {
+                    Console.WriteLine("now showing PPG");
+                }
+            }
+
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (InvalidDataException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
+
